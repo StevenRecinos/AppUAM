2568727655cb00fa0cf6a6030ac314565ac62cb4
Dennis Benavides <dennisb@uassistme.com>
2021-07-22 08:35:04 -0600

    primer commit

--- ConectarBD.cs
+++ ConectarBD.cs
@@ -11,77 +11,161 @@ namespace App
 {
     public class ConectarBD
     {
-        public static MySqlConnection abrirconexion()
+        const String stringSQL = "server=172.16.0.21;user id=root;password=U@ssistm3;persist security info=True;database=prueba";
+        DataSet ds = new DataSet();
+        MySql.Data.MySqlClient.MySqlConnection connection = new MySql.Data.MySqlClient.MySqlConnection(stringSQL);
+        MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand();
+
+        public static string valor;
+
+        private bool OpenConnection()
         {
-
-         MySqlConnection cn = new MySqlConnection("server=localhost; Database=prueba; persist security info=True; User Id =root; password=");
-        cn.Open();
-        return cn;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            /*
-            const String stringSQL = "server=localhost;user id=root;password=;persist security info=True;database=prueba";
-            DataSet ds = new DataSet();
-            MySql.Data.MySqlClient.MySqlConnection connection = new MySql.Data.MySqlClient.MySqlConnection(stringSQL);
-            MySql.Data.MySqlClient.MySqlCommand command = new MySql.Data.MySqlClient.MySqlCommand();
-
-            public static string valor;
-
-            public bool OpenConnection()
+            try
             {
-                try
-                {
-                    connection.Open();
-                    return true;
-                }
-                catch (MySqlException ex)
-                {
-                    //When handling errors, you can your application's response based 
-                    //on the error number.
-                    //The two most common error numbers when connecting are as follows:
-                    //0: Cannot connect to server.
-                    //1045: Invalid user name and/or password.
-                    switch (ex.Number)
-                    {
-                        case 0:
-                            MessageBox.Show("Cannot connect to server.  Contact administrator");
-                            break;
-
-                        case 1045:
-                            MessageBox.Show("Invalid username/password, please try again");
-                            break;
-                    }
-                    return false;
-                }
+                connection.Open();
+                return true;
             }
-
-            private bool CloseConnection()
+            catch (MySqlException ex)
             {
-                try
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
                 {
-                    connection.Close();
-                    return true;
+                    case 0:
+                        MessageBox.Show("Cannot connect to server.  Contact administrator");
+                        break;
+
+                    case 1045:
+                        MessageBox.Show("Invalid username/password, please try again");
+                        break;
                 }
-                catch (MySqlException ex)
-                {
-                    MessageBox.Show(ex.Message);
-                    return false;
-                }
-            */
+                return false;
+            }
         }
 
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+
+        //Insert statement
+        public void Insert(string query)
+        {
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        //Update statement
+        public void Update(string query)
+        {
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //create mysql command
+                MySqlCommand cmd = new MySqlCommand();
+                //Assign the query using CommandText
+                cmd.CommandText = query;
+                //Assign the connection using Connection
+                cmd.Connection = connection;
+
+                //Execute query
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        //Delete statement
+        public void Delete(string query)
+        {
+            if (this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+        }
+
+        public DataTable Select(string query)
+        {
+            DataTable data = null;
+
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                cmd.Dispose();
+                if (dataReader != null)
+                {
+                    data = new DataTable();
+                    data.Load(dataReader);
+                }
+                else
+                {
+                    throw new Exception();
+                }
+                dataReader.Dispose();
+                return data;
+            }
+            else
+            {
+                return data;
+            }
+        }
+        public object SelectValue(string query)
+        {
+            object value = null;
+            //Open connection
+            if (this.OpenConnection() == true)
+            {
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                value = cmd.ExecuteScalar();
+                cmd.Dispose();
+                this.CloseConnection();
+                return value;
+            }
+            else
+            {
+                return value;
+            }
+        }
+
+        public void ingresar_valor(string v)
+        {
+            valor = v;
+        }
+        public string obtener_valor()
+        {
+            return valor;
+        }
     }
 } 
